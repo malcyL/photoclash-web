16df4193bb811cc52fe9f69c52cbde4705f9dd5f
MalcyL <malcolm@landonsonline.me.uk>
2020-01-28 20:06:23 +0000

    Angular token config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { AngularTokenModule } from 'angular-token';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -20,14 +20,18 @@ import { MatIconModule } from '@angular/material/icon';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatButtonModule,
     MatSidenavModule,
     MatToolbarModule,
     MatTooltipModule,
     MatListModule,
-    MatIconModule
+    MatIconModule,
+    AngularTokenModule.forRoot({
+      // ...
+    })
   ],
-  providers: [],
+  providers: [AngularTokenModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
