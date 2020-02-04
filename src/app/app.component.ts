d2743df35d504450f76a4e5d826bfcf85196eef7
MalcyL <malcolm@landonsonline.me.uk>
2020-02-04 20:59:43 +0000

    Plug in initial component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+
+import { IAppState } from './store/state/app.state';
+import { GetCompetitions } from './store/actions/competition.actions';
+import { selectCompetitionList } from './store/selectors/competition.selectors';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'photoclash-web';
+
+  competitionList = this.store.pipe(select(selectCompetitionList));
+
+  constructor(private store: Store<IAppState>) {}
+
+  ngOnInit() {}
 }
