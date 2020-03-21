b8ead627fe2e24740c664d7306ca3fcd44ae32a6
MalcyL <malcolm@landonsonline.me.uk>
2020-03-21 08:56:25 +0000

    Tidy src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+// Angular Imports
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -5,27 +6,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { AngularTokenModule } from 'angular-token';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+// NGRX Imports
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
-import { appReducers } from './store/reducers/app.reducers';
-import { environment } from '../environments/environment';
-
-import { CompetitionEffects } from './store/effects/competition.effects';
-import { CompetitionService } from './services/competition.service';
-import { CompetitionsComponent } from './components/competitions/competitions.component';
-
-import { AuthEffects } from './store/effects/auth.effects';
-import { AuthComponent } from './components/auth/auth.component';
-import { LoginComponent } from './components/auth/login/login.component';
-
-
+// Angular Material Imports
 import { MatButtonModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -36,22 +23,51 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material';
+
+// Other Library Imports
+import { AngularTokenModule } from 'angular-token';
+
+// Angular Project Import
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { appReducers } from './store/reducers/app.reducers';
+
+// Authentication Imports
+import { AuthEffects } from './store/effects/auth.effects';
+import { AuthComponent } from './components/auth/auth.component';
+import { LoginComponent } from './components/auth/login/login.component';
 import { LogoutMenuButtonComponent } from './components/auth/logout-menu-button/logout-menu-button.component';
 import { OauthCallbackComponent } from './components/auth/oauth-callback/oauth-callback.component';
+
+// Competitions Imports
+import { CompetitionEffects } from './store/effects/competition.effects';
+import { CompetitionService } from './services/competition.service';
+import { CompetitionsComponent } from './components/competitions/competitions.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompetitionsComponent,
     AuthComponent,
+    CompetitionsComponent,
     LoginComponent,
     LogoutMenuButtonComponent,
     OauthCallbackComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    // TODO: Angular token config could come from a file
+    AngularTokenModule.forRoot({
+      apiBase: `${environment.angularToken.apiBase}`,
+      signInStoredUrlStorageKey: 'originalUrl',
+      signInRedirect: '/login',
+      oAuthBase: `${environment.angularToken.oauthBase}`,
+      oAuthWindowType: 'sameWindow',
+    }),
     BrowserAnimationsModule,
+    BrowserModule,
+    EffectsModule.forRoot([AuthEffects, CompetitionEffects]),
     FlexLayoutModule,
     FormsModule,
     HttpClientModule,
@@ -65,15 +81,7 @@ import { OauthCallbackComponent } from './components/auth/oauth-callback/oauth-c
     MatSidenavModule,
     MatToolbarModule,
     MatTooltipModule,
-    AngularTokenModule.forRoot({
-      apiBase: `${environment.angularToken.apiBase}`,
-      signInStoredUrlStorageKey: 'originalUrl',
-      signInRedirect: '/login',
-      oAuthBase: `${environment.angularToken.oauthBase}`,
-      oAuthWindowType: 'sameWindow',
-    }),
     StoreModule.forRoot(appReducers),
-    EffectsModule.forRoot([AuthEffects, CompetitionEffects]),
     StoreRouterConnectingModule.forRoot({ stateKey: 'router' }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
